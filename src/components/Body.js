649f19553f6bbe9659af10b488f799d964089f91
Rantej <rantejpatil1721@gmail.com>
2024-08-06 13:09:51 +0530

    ep - 10 Added Tailwind css

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,6 @@
 
+
+
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
@@ -45,17 +47,21 @@ const Body = () => {
     <Shimmer />
   ) : (
     <div className="body">
-      <div className="filter">
-        <div className="search">
+      {/* <div className="filter">
+        <div className="search"> */}
+      <div className="filter flex">
+      <div className="search m-4 p-4"> 
           <input
             type="text"
-            className="search-box"
+            // className="search-box"
+            className="border border-solid border-black"
             value={searchText}
             onChange={(e) => {
               setSearchText(e.target.value);
             }}
           />
           <button
+          className="px-4 py-2 bg-green-100 m-4 rounded-lg"
             onClick={() => {
               const filteredRestaurant = listOfRestaurants.filter((res) =>
                 res.info.name.toLowerCase().includes(searchText.toLowerCase())
@@ -65,8 +71,9 @@ const Body = () => {
           >
             Search
           </button>
-        </div>
-        <button
+          </div>  
+       {/* </div>
+         <button
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
@@ -76,9 +83,24 @@ const Body = () => {
           }}
         >
           Top Rated Restaurants
-        </button>
-      </div>
-      <div className="res-container">
+        </button> 
+      </div>*/}
+        <div className="search m-4 p-4 flex items-center">
+          <button
+            className="px-4 py-2 bg-gray-100 rounded-lg"
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter(
+                (res) => res.info.avgRating > 4
+              );
+              setFilteredRestaurant(filteredList); // Update only the filteredRestaurant state
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
+        </div>  
+      {/* <div className="res-container"> */}
+      <div className="flex flex-wrap">
         {/* /////filteredRestaurants */}
         {filteredRestaurants.map((restaurant) => (
           <Link  
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -11,13 +11,17 @@ const RestaurantCard = (props) => {
       sla: { deliveryTime }
     } = resData?.info || {};
     return(
-      <div className="res-card">
+      // <div className="res-card">
+      <div className="m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200">
         {/* <img className="res-logo"
          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,//f_auto,q_auto,w_660/rvxp5xbniat84r6efku2"/> */}
-          <img className="res-logo"
+          {/* <img className="res-logo" */}
+          <img 
+        className="rounded-lg"
          src={CDN_URL + cloudinaryImageId}/>
   
-        <h3> {name} </h3>
+        {/* <h3> {name} </h3> */}
+        <h3 className="font-bold py-4 text-lg">{name}</h3>
         <h4>{cuisines.join(", ")}</h4>
         <h4>{avgRating} stars</h4>
         <h4>{costForTwo} </h4>
